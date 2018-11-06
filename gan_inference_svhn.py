0362a5e3d6466bf56d36ebc204f951e229f66573
caozhangjie <caozhangjie14@gmail.com>
2018-11-07 00:23:02 +0800

    init

---
+++ gan_inference_svhn.py
@@ -0,0 +1,243 @@
+from torchvision.datasets import SVHN
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
+from net_svhn import Extractor, Discriminator, Generator, Discriminator1
+import torch
+import torch.nn as nn
+import numpy as np
+import torch.optim as optim
+import pdb
+import os
+import plot
+import save_images
+import time
+
+def generate_image(frame, fixed_noise, model, method, outf):
+    x, x_mean, x_std = model(fixed_noise)
+    samples = ((x.cpu()+1.0)*255.0/2.0).detach().numpy().astype('int32')
+    save_images.save_images(
+        samples.reshape((-1, 3, 32, 32)), 
+        os.path.join(outf, '{}/{:06d}_samples.png'.format(method, frame))
+    )
+
+os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+
+test_interval = 5000
+print_interval = 100
+batch_size = 64
+std = .1
+method = 'ali'
+if method in ['vegan-kl', 'vegan-ikl', 'vegan-jsd']:
+    type_q = 'learn_std' # learn_std, fix_std, no_std
+    type_p = 'no_std'
+    z_samples = 100 # MC estimation for D(q(z)||p(z))
+elif method == 'vae':
+    tpye_q = 'learn_std'
+    type_p = 'learn_std'
+else:
+    type_q = 'no_std'
+    type_p = 'no_std'
+d_list = ['alice', 'alice-z', 'alice-x', 'vegan', 'vegan-wgan-gp', 'vegan-kl', 'vegan-ikl', 'vegan-jsd', 'vegan-mmd']
+if method in d_list:
+    distance_x = 'l2' # l1, l2
+lambda_ = 1. # Balance reconstruction and regularization in vegan
+
+dim = 64 # Model dimensionality
+output_dim = 3072 # Number of pixels in svhn (3*32*32)
+
+transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5))
+    ])
+train_dset = SVHN('./data/svhn', 'train', download=True, transform=transform)
+test_dset = SVHN('./data/svhn', 'test', download=True, transform=transform)
+train_loader = DataLoader(train_dset, num_workers=4, pin_memory=True, batch_size=64, shuffle=True, drop_last=True)
+test_loader = DataLoader(test_dset, num_workers=4, pin_memory=True, batch_size=1, shuffle=False)
+
+num_iter = 200000
+dim_latent = 128
+lr=2e-4
+
+os.system('mkdir -p ./log/'+method)
+outf = './log/'
+logfile = './log/'+method+'/log.txt'
+open(logfile, 'w')
+
+if method in ['vae']:
+    beta1 = 0.9
+else:
+    beta1=0.5
+
+if method in ['vegan', 'vegan-wgan-gp', 'vegan-kl', 'vegan-jsd', 'vegan-ikl']:
+    bn_falg = False # Use batch_norm or not
+    dim_latent = 8 # Dimensionality of the latent z
+else:
+    bn_flag = False
+    dim_latent = 128
+n_vis = batch_size*2 # Number of samples to be visualized
+
+if method in ['vegan-mmd', 'vegan-kl', 'vegan-ikl', 'vegan-jsd', 'vae']:
+    critic_iters = 0 # No discriminators
+elif method in ['vegan', 'vegan-wgan-gp', 'wali', 'wali-gp']:
+    critic_iters = 5 # 5 iters of D per iter of G
+else:
+    critic_iters = 1
+
+extractor = Extractor(type_q=type_q)
+extarctor = extractor.cuda()
+extractor.apply()
+if method in ['vegan', 'vegan-wgan-gp']:
+    discriminator = Discriminator1()
+elif method in ['vegan-mmd', 'vegan-kl', 'vegan-ikl', 'vegan-jsd', 'vae']:
+    pass
+else:
+    discriminator = Discriminator()
+discriminator = discriminator.cuda()
+generator = Generator()
+generator = generator.cuda()
+
+criterion = nn.BCELoss()
+
+
+optimizer_e = optim.Adam(extractor.parameters(), lr=lr, betas=(beta1, 0.999))
+optimizer_d = optim.Adam(discriminator.parameters(), lr=lr, betas=(beta1, 0.999))
+optimizer_g = optim.Adam(generator.parameters(), lr=lr, betas=(beta1, 0.999))
+
+
+if method in ['ali','vae','wali', 'wali-gp']:
+    rec_penalty = None
+elif method == 'alice-z':
+    rec_penalty = 1.*lib.utils.distance.distance(real_x, rec_x, DISTANCE_X)
+elif method == 'alice-x':
+    rec_penalty = 1.*lib.utils.distance.distance(p_z, rec_z, DISTANCE_X)
+elif method == 'alice':
+    rec_penalty = 1.*lib.utils.distance.distance(real_x, rec_x, DISTANCE_X)
+    rec_penalty += 1.*lib.utils.distance.distance(p_z, rec_z, DISTANCE_X)
+elif method == 'vegan':
+    rec_penalty = 1.*lib.utils.distance.distance(real_x, rec_x, DISTANCE_X)
+elif method == 'vegan-wgan-gp':
+    rec_penalty = 1.*lib.utils.distance.distance(real_x, rec_x, DISTANCE_X)
+elif method == 'vegan-mmd':
+    rec_penalty = 1.*lib.utils.distance.distance(real_x, rec_x, DISTANCE_X)
+elif method == 'vegan-kl':
+    rec_penalty = 1.*lib.utils.distance.distance(real_x, rec_x, DISTANCE_X)
+elif method == 'vegan-ikl':
+    rec_penalty = 1.*lib.utils.distance.distance(real_x, rec_x, DISTANCE_X)
+elif method == 'vegan-jsd':
+    rec_penalty = 1.*lib.utils.distance.distance(real_x, rec_x, DISTANCE_X)
+else:
+    raise('NotImplementedError')
+
+fixed_noise = torch.randn([n_vis, dim_latent]).cuda()
+len_loader = len(train_loader)
+
+for iter_ in range(num_iter):
+    start_time = time.time()
+    for sub_iter in range(critic_iters + 1):
+        true_iter = iter_ * (critic_iters + 1) + sub_iter
+        if true_iter % len_loader == 0:
+            iter_loader = iter(train_loader)
+        data = iter_loader.next()
+        input_, label = data
+        input_ = input_.cuda()
+        #label = label.cuda()
+        real_x = input_
+        q_z, q_z_mean, q_z_std = extractor(input_)
+        rec_x, rex_x_mean, rec_x_std = generator(q_z)
+        p_z = torch.randn([input_.size(0), dim_latent]).cuda()
+        fake_x, _, _ = generator(p_z)
+        rec_z, _, _ = extractor(fake_x)
+        if method in ['vegan-kl', 'vegan-ikl', 'vegan-jsd']:
+            p_z_mean = torch.zeros([z_samples, dim_latent]).float().cuda()
+            p_z_std = torch.ones([z_samples, dim_latent]).float().cuda()
+        elif method == 'vae':
+            p_z_mean = torch.zeros([64, dim_latent]).float().cuda()
+            p_z_std = torch.ones([64, dim_latent]).float().cuda()
+        if method in ['vegan', 'vegan-wgan-gp']:
+            disc_real = discriminator(p_z)
+            disc_fake = discriminator(q_z)
+        elif method in ['vegan-mmd', 'vegan-kl', 'vegan-ikl', 'vegan-jsd', 'vae']:
+            pass
+        else:
+            disc_real = discriminator(real_x, q_z)
+            disc_fake = discriminator(fake_x, p_z)
+        if method == 'ali':
+            label_dis = torch.cat((torch.zeros([input_.size(0)]), torch.ones([input_.size(0)])), dim=0).cuda()
+            label_gen = torch.cat((torch.ones([input_.size(0)]), torch.zeros([input_.size(0)])), dim=0).cuda()
+            disc_input = torch.cat((disc_real, disc_fake), dim=0)
+            disc_input = nn.Sigmoid()(disc_input).view(-1)
+            if sub_iter == 0:
+                disc_loss = criterion(disc_input, label_gen.view(-1))
+                optimizer_g.zero_grad()
+                optimizer_e.zero_grad()
+                disc_loss.backward()
+                optimizer_g.step()
+                optimizer_e.step()
+            else:
+                disc_loss = criterion(disc_input, label_dis.view(-1))
+                optimizer_d.zero_grad()
+                disc_loss.backward()
+                optimizer_d.step()
+    if method in ['vegan-mmd', 'vegan-kl', 'vegan-ikl', 'vegan-jsd', 'vae']:
+        if iter_ > 0:
+            plot.plot('train gen cost ', gen_loss.cpu().item())
+    else:
+        plot.plot('train disc cost', disc_loss.cpu().item())
+    plot.plot('time', time.time() - start_time)
+    '''
+    if iter_ % 100 == 99:
+        if rec_penalty is not None:
+            dev_rec_costs = []
+            dev_reg_costs = []
+            for images,_ in dev_gen():
+                _dev_rec_cost, _dev_gen_cost = session.run(
+                    [rec_penalty, gen_cost], 
+                    feed_dict={real_x_int: images}
+                )
+                dev_rec_costs.append(_dev_rec_cost)
+                dev_reg_costs.append(_dev_gen_cost - _dev_rec_cost)
+            plot.plot('dev rec cost', np.mean(dev_rec_costs))
+            plot.plot('dev reg cost', np.mean(dev_reg_costs))
+        else:
+            dev_gen_costs = []
+            for images,_ in dev_gen():
+                _dev_gen_cost = session.run(
+                    gen_cost, 
+                    feed_dict={real_x_int: images}
+                )
+                dev_gen_costs.append(_dev_gen_cost)
+            plot.plot('dev gen cost', np.mean(dev_gen_costs))
+    '''
+    # Write logs
+    if (iter_ < 5) or (iter_ % print_interval == print_interval-1):
+        plot.flush(os.path.join(outf, method), logfile)
+    plot.tick()
+
+    # Generation and reconstruction
+    if iter_ % test_interval == test_interval-1:
+        generator.eval()
+        discriminator.eval()
+        extractor.eval()
+        generate_image(iter_, fixed_noise, generator, method, outf)
+        ori_images = []
+        gen_images = []
+        for batch_id, data in enumerate(test_loader):
+            input_, label = data
+            input_ = input_.cuda()
+            sample, sample_mean, sample_std = extractor(input_)
+            rec_x, rex_x_mean, rec_x_std = generator(sample)
+            ori_images.append(((input_.cpu()+1.0)*255.0/2).detach().numpy().astype('int32'))
+            gen_images.append(((rec_x.cpu()+1.0)*255.0/2).detach().numpy().astype('int32'))
+        ori_images = np.array(ori_images)
+        gen_images = np.array(gen_images)
+        save_images.save_images(
+            ori_images.reshape((-1, 3, 32, 32)), 
+            os.path.join(outf, '{}/{:06d}_origin.png'.format(method, iter_))
+        )
+        save_images.save_images(
+            gen_images.reshape((-1, 3, 32, 32)), 
+            os.path.join(outf, '{}/{:06d}_gen.png'.format(method, iter_))
+        )
+        generator.train()
+        discriminator.train()
+        extractor.train()
---
+++ net_svhn.py
@@ -0,0 +1,211 @@
+import torch
+import torch.nn as nn
+import pdb
+
+def init_weights(m):
+    classname = m.__class__.__name__
+    if classname.find('Conv2d') != -1 or classname.find('Linear') != -1 or classname.find('ConvTranspose2d') != -1:
+        nn.init.kaiming_uniform_(m.weight)
+        nn.init.zeros_(m.bias)
+    elif classname.find('BatchNorm') != -1:
+        m.weight.data.normal_(1.0, 0.02)
+        m.bias.data.fill_(0)
+
+class Discriminator1(nn.Module):
+    def __init__(self, dim_latent=128, bn_flag=True):
+        super(Discriminator1, self).__init__()
+        self.dim_latent = dim_latent
+        self.linear1 = nn.Linear(dim_latent, 1024)
+        if self.bn_flag:
+            self.bn1 = nn.BatchNorm1d(1024)
+        self.relu1 = nn.LeakyReLU(0.2)
+        
+        self.linear2 = nn.Linear(1024, 512)
+        if self.bn_flag:
+            self.bn2 = nn.BatchNorm1d(512)
+        self.relu2 = nn.LeakyReLU(0.2)
+
+        self.linear3 = nn.Linear(512, 256)
+        if self.bn_flag:
+            self.bn3 = nn.BatchNorm1d(256)
+        self.relu3 = nn.LeakyReLU(0.2)
+
+        self.linear4 = nn.Linear(256, 256)
+        if self.bn_flag:
+            self.bn4 = nn.BatchNorm1d(256)
+        self.relu4 = nn.LeakyReLU(0.2)
+
+        self.linear5 = nn.Linear(256, 1)
+
+    def forward(self, x):
+        noise = (torch.randn(x.size()) * 0.3).cuda()
+        x = self.linear1(x + noise)
+        if self.bn_flag:
+            x = self.bn1(x)
+        x = self.relu1(x)
+        noise = (torch.randn(x.size()) * 0.5).cuda()
+        x = self.linear2(x + noise)
+        if self.bn_flag:
+            x = self.bn2(x)
+        x = self.relu2(x)
+        noise = (torch.randn(x.size()) * 0.5).cuda()
+        x = self.linear3(x + noise)
+        if self.bn_flag:
+            x = self.bn3(x)
+        x = self.relu3(x)
+        noise = (torch.randn(x.size()) * 0.5).cuda()
+        x = self.linear4(x + noise)
+        if self.bn_flag:
+            x = self.bn4(x)
+        x = self.relu4(x)
+        x = self.linear5(x)
+        return x.view(-1)
+
+class Generator(nn.Module):
+    def __init__(self, dim_latent=128, dim=64, bn_flag=True):
+        super(Generator, self).__init__()
+        self.dim_latent = dim_latent
+        self.bn_flag = bn_flag
+        self.dim = dim
+        self.linear1 = nn.Linear(dim_latent, 4*4*4*dim)
+        if bn_flag:
+            self.bn1 = nn.BatchNorm1d(4*4*4*dim)
+        self.relu1 = nn.ReLU()
+        self.deconv1 = nn.ConvTranspose2d(4*dim, 2*dim, 5, 2, 2, 1)
+        if bn_flag:
+            self.bn2 = nn.BatchNorm2d(2*dim)
+        self.relu2 = nn.ReLU()
+        self.deconv2 = nn.ConvTranspose2d(2*dim, dim, 5, 2, 2, 1)
+        if bn_flag:
+            self.bn3 = nn.BatchNorm2d(dim)
+        self.relu3 = nn.ReLU()
+        self.deconv3 = nn.ConvTranspose2d(dim, 3, 5, 2, 2, 1)
+        self.tanh = nn.Tanh()
+
+    def forward(self, x):
+        x = x.view(-1, self.dim_latent)
+        x = self.linear1(x)
+        if self.bn_flag:
+            x = self.bn1(x)
+        x = self.relu1(x)
+        x = x.view(-1, 4*self.dim, 4, 4)
+        x = self.deconv1(x)
+        if self.bn_flag:
+            x = self.bn2(x)
+        x = self.relu2(x)
+        x = x.view(-1, 2*self.dim, 8, 8)
+        x = self.deconv2(x)
+        if self.bn_flag:
+            x = self.bn3(x)
+        x = self.relu3(x)
+        x = x.view(-1, self.dim, 16, 16)
+        x = self.deconv3(x)
+        x = self.tanh(x)
+        return x, None, None
+
+class Extractor(nn.Module):
+    def __init__(self, dim_latent=128, dim=64, bn_flag=True, type_q='learn_std', std=0.1):
+        super(Extractor, self).__init__()
+        self.std = std
+        self.type_q = type_q
+        self.dim_latent = dim_latent
+        self.bn_flag = bn_flag
+        self.dim = dim
+        self.pad1 = nn.ZeroPad2d((1, 2, 1, 2))
+        self.conv1 = nn.Conv2d(3, dim, 5, 2)
+        self.relu1 = nn.LeakyReLU(0.2)
+
+        self.pad2 = nn.ZeroPad2d((1, 2, 1, 2))
+        self.conv2 = nn.Conv2d(dim, dim*2, 5, 2)
+        if bn_flag:
+            self.bn1 = nn.BatchNorm2d(2*dim)
+        self.relu2 = nn.LeakyReLU(0.2)
+
+        self.pad3 = nn.ZeroPad2d((1, 2, 1, 2))
+        self.conv3 = nn.Conv2d(dim*2, dim*4, 5, 2)
+        if bn_flag:
+            self.bn2 = nn.BatchNorm2d(4*dim)
+        self.relu3 = nn.LeakyReLU(0.2)
+        self.linear = nn.Linear(4*4*4*dim, dim_latent)
+
+    def forward(self, x):
+        x = self.pad1(x)
+        x = self.conv1(x)
+        x = self.relu1(x)
+        x = self.pad2(x)
+        x = self.conv2(x)
+        if self.bn_flag:
+            x = self.bn1(x)
+        x = self.relu2(x)
+        x = self.pad3(x)
+        x = self.conv3(x)
+        if self.bn_flag:
+            x = self.bn2(x)
+        x = self.relu3(x)
+        x = x.view(-1, 4*4*4*self.dim)
+        
+        if self.type_q == 'learn_std':
+            std = self.linear(x)
+            std = torch.exp(std)
+        elif self.type_q == 'fix_std':
+            std = (torch.ones([x.size(0), self.dim_latent]) * self.std).cuda()
+        else:
+            std = None
+            mean = None
+        x = self.linear(x)
+
+        if self.type_q in ['learn_std', 'fix_std']:
+            epsilon = torch.randn([x.size(0), self.dim_latent]).cuda()
+            mean = x
+            std = mean + std * epsilon
+
+        return x, mean, std
+
+class Discriminator(nn.Module):
+    def __init__(self, dim_latent=128, dim=64, bn_flag=True, dr_rate=0.2):
+        super(Discriminator, self).__init__()
+        self.dim=dim
+        self.pad1 = nn.ZeroPad2d((1,2,1,2))
+        self.conv1 = nn.Conv2d(3, dim, 5, 2)
+        self.relu1 = nn.LeakyReLU(0.2)
+        self.drop1 = nn.Dropout(dr_rate)
+        self.pad2 = nn.ZeroPad2d((1,2,1,2))
+        self.conv2 = nn.Conv2d(dim, 2*dim, 5, 2)
+        self.relu2 = nn.LeakyReLU(0.2)
+        self.drop2 = nn.Dropout(dr_rate)
+        self.pad3 = nn.ZeroPad2d((1,2,1,2))
+        self.conv3 = nn.Conv2d(2*dim, 4*dim, 5, 2)
+        self.relu3 = nn.LeakyReLU(0.2)
+        self.drop3 = nn.Dropout(dr_rate)
+
+        self.linear1 = nn.Linear(dim_latent, 512)
+        self.relu4 = nn.LeakyReLU(0.2)
+        self.drop4 = nn.Dropout(dr_rate)
+
+        self.linear2 = nn.Linear(512+4*4*4*dim, 512)
+        self.relu5 = nn.LeakyReLU(0.2)
+        self.drop5 = nn.Dropout(dr_rate)
+
+        self.linear3 = nn.Linear(512, 1)
+
+    def forward(self, x, z):
+        x = self.pad1(x)
+        x = self.conv1(x)
+        x = self.relu1(x)
+        x = self.drop1(x)
+        x = self.pad2(x)
+        x = self.conv2(x)
+        x = self.relu2(x)
+        x = self.drop2(x)
+        x = self.pad3(x)
+        x = self.conv3(x)
+        x = self.relu3(x)
+        x = self.drop3(x)
+        z = self.drop4(self.relu4(self.linear1(z)))
+        x = x.view(-1, 4*4*4*self.dim)
+        x = torch.cat((x, z), dim=1)
+        x = self.linear2(x)
+        x = self.relu5(x)
+        x = self.drop5(x)
+        x = self.linear3(x)
+        return x
